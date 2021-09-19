95ac3fc38a9ea04956c5cea48db319ba19881a79
Supervisor <Supervisor@DESKTOP-BJX>
2021-09-19 20:15:57 +0800

    添加项目文件。

---
+++ CMS/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CMS.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route()]
+        [Route("Index")]
+        [Route("Home/Index")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Editor")]
+        [Route("Home/Editor")]
+        public ActionResult Editor()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ CMS/Controllers/UEditorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UEditor.Core;
+
+namespace CMS.Controllers
+{
+    public class UEditorController : Controller
+    {
+        public ContentResult Upload()
+        {
+            var response = UEditorService.Instance.UploadAndGetResponse(HttpContext.ApplicationInstance.Context);
+            return Content(response.Result, response.ContentType);
+        }
+    }
+}
